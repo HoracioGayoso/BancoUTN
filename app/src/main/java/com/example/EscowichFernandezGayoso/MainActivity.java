3d3b19e6aac8ce726e39f6c5185715991e783fe3
HoracioGayoso <horaciogayoso9@gmail.com>
2022-09-16 12:18:25 -0300

    Se crea Activity, y se practica con algunos widgets

--- app/src/main/java/com/example/EscowichFernandezGayoso/MainActivity.java
+++ app/src/main/java/com/example/EscowichFernandezGayoso/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.EscowichFernandezGayoso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinnerMonedas = (Spinner) findViewById(R.id.spinner3);
+        spinnerMonedas.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.datos)));
     }
+
 }
