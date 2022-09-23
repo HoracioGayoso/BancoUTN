1cb89fff06a9f8670defc65bb1b202bb2a19f098
HoracioGayoso <horaciogayoso9@gmail.com>
2022-09-23 20:34:48 -0300

    Se configura el binding y se termina la parte grafica de la pantalla 1 "Construir plazo fijo"

--- app/src/main/java/com/example/EscowichFernandezGayoso/MainActivity.java
+++ app/src/main/java/com/example/EscowichFernandezGayoso/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.EscowichFernandezGayoso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.EscowichFernandezGayoso.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Spinner spinnerMonedas = (Spinner) findViewById(R.id.spinner3);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        Spinner spinnerMonedas = binding.spinnerMonedas;
         spinnerMonedas.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.datos)));
     }
 
